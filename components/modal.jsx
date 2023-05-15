38de6688455b1bfa1d5512208a6ce5b7f3ce5129
arubaito <asdfghjk0330@gmail.com>
2023-05-15 17:00:07 +0900

    トップページにモーダルウィンドウのサンプルを構築

---
+++ components/modal.jsx
@@ -0,0 +1,29 @@
+/* 
+    汎用モーダルウィンドウ
+    ※ 利用側でstate変数を定義してコンポーネントのpropsへ渡す
+    show : モーダルウィンドウを開閉するstate変数
+    setShow : state変数を更新する関数
+    children : モーダルウィンドウで表示する中身
+    @see https://reffect.co.jp/react/react-modal
+ */
+export default function Modal({show=false, setShow, children}){
+    
+    // モーダルウィンドウを閉じる関数
+    const closeModal = () => setShow(false);
+    
+    // モーダルウィンドウの表示非表示を変数showで切り替え
+    if(show){
+        return (
+            <div>
+                <div id="overlay" onClick={closeModal}>
+                <div id="content" onClick={(e) => e.stopPropagation()}>
+                    {children}
+                    <p><button onClick={closeModal}>close</button></p>
+                </div>
+                </div>
+            </div>
+        );
+    } else {
+        return null;
+    }
+}
