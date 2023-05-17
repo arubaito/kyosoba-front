b03dec57815320df5b4290fc7db85514e2e59a77
arubaito <asdfghjk0330@gmail.com>
2023-05-17 22:46:41 +0900

    ヘッダとフッタ以外に背景画像を表示する処理を追加

--- components/common/layout.jsx
+++ components/common/layout.jsx
@@ -1,6 +1,7 @@
 import Header from "components/common/header";
 import Footer from "components/common/footer";
 import Container from "./Container";
+import styles from "styles/common_components/Layout.module.css"
 
 /* 
     ページ共通のヘッダやフッタのレイアウトを表示する 
@@ -10,9 +11,11 @@ export default function Layout({children}){
     return (
         <>
             <Header />
-                <Container>
-                    {children}
-                </Container>
+                <div className={styles.backgroundContainer}>
+                    <Container>
+                        {children}
+                    </Container>
+                </div>
             <Footer />
         </>
 
