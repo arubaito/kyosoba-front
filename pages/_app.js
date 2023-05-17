2b38a632c56bdd8fa4c4203c1919c98259a858b9
arubaito <asdfghjk0330@gmail.com>
2023-05-17 11:21:06 +0900

    Font Awesomeの設定追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import Layout from 'components/common/layout';
 import 'styles/globals.css'
 
+// Font Awesomeの設定
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import {config} from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+
 /* 
   全ページに反映させたい内容はカスタムAPPコンポーネント(_app.js)を使う
   ヘッダ、フッタを表示させるLayoutコンポーネントを使ってページコンポーネントを囲む
