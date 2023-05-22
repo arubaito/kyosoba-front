4aa6939f94daa2e8e5a96b49c8a46c087ccc82cf
arubaito <asdfghjk0330@gmail.com>
2023-05-22 10:51:33 +0900

    競走馬詳細情報画面について、サーバのAPIをたたく関数と、SSGで関数を使って情報を取得する処理を組み込み

--- pages/horse/index.jsx
+++ pages/horse/index.jsx
@@ -3,8 +3,23 @@ import styles from 'styles/page/horse.module.css';
 import sodashi from 'public/image/sodashi.jpg'; // 画像をimportするとNext.jsは色々便利
 import UmaInfoIconGroup from 'components/utils/UmaInfoIcon/UmaInfoIconGroup';
 import UmaInfoName from 'components/utils/UmaInfoName/UmaInfoName';
+import { getKyosobaInfo } from 'lib/api';
 
-export default function Horse({ horseName }) {
+/*
+    競走馬詳細情報画面
+    getServerSidePropsでサーバから取得してきた競走馬の情報を表示
+ */
+export default function Horse({ 
+    name,
+    birthday,
+    kyusha,
+    seisansyla,
+    jockey,
+    banushi,
+    tsusanseiseki,
+    syokin,
+    ketto,
+    }) {
     return (
         <>
             <div className={styles.horseInfo}>
@@ -27,4 +42,29 @@ export default function Horse({ horseName }) {
             </div>
         </>
     );
-} 
+}
+
+/* SSG */
+export async function getServerSideProps(){
+    
+    console.log("horse/index.jsx#getServerSideProps")
+
+    // サーバから競走馬の情報を取得
+    const response = await getKyosobaInfo();
+    console.log(response);
+
+    // 競走馬の各情報をオブジェクト形式に
+    return {
+        props:{
+            name:response.name,
+            birthday:response.birthday,
+            kyusha: response.kyusha,
+            seisansya: response.seisansya,     
+            jockey: response.jockey,
+            banushi: response.banushi,
+            tsusanseiseki: response.tsusanseiseki,
+            syokin: response.syokin,
+            ketto: response.ketto,
+        },
+    }
+}
