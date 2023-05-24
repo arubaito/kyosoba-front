095c98dd48f47539af678719970457c61b5208d3
arubaito <asdfghjk0330@gmail.com>
2023-05-25 08:57:09 +0900

    血統表に母方の血統も表示

--- components/KettouhyouSyousai.jsx
+++ components/KettouhyouSyousai.jsx
@@ -77,7 +77,71 @@ export default function KettouhyouSyousai() {
                         <td>Querida</td>
                     </tr>
                     {/* 母方 */}
-                    
+                    <tr>
+                        <td rowSpan="16">エピファネイア</td>
+                        <td rowSpan="8">シンボリクリスエス</td>
+                        <td rowSpan="4">Kris.S</td>
+                        <td rowSpan="2">A</td>
+                        <td>Roberto</td>
+                    </tr>
+                    <tr>
+                        <td>Sharp Queen</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="2">Tee Kay</td>
+                        <td>Gold Meridian</td>
+                    </tr>
+                    <tr>
+                        <td>Sharp Queen</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="4">Tee Kay</td>
+                        <td rowSpan="2">Tee Kay</td>
+                        <td>Tri Argo</td>
+                    </tr>
+                    <tr>
+                        <td>Sharp Queen</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="2">Tee Kay</td>
+                        <td>Gold Meridian</td>
+                    </tr>
+                    <tr>
+                        <td>Sharp Queen</td>
+                    </tr>
+
+                    <tr>
+                        <td rowSpan="8">シーザリオ</td>
+                        <td rowSpan="4">シーザリオ</td>
+                        <td rowSpan="2">スペシャルウィーク</td>
+                        <td>サンデーサイレンス</td>
+                    </tr>
+                    <tr>
+                        <td>キャンペーンガール</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="2">キロフプリミエール</td>
+                        <td>Sadler's Wells</td>
+                    </tr>
+                    <tr>
+                        <td>Querida</td>
+                    </tr>
+
+                    <tr>
+                        <td rowSpan="4">シーザリオ</td>
+                        <td rowSpan="2">スペシャルウィーク</td>
+                        <td>サンデーサイレンス</td>
+                    </tr>
+                    <tr>
+                        <td>キャンペーンガール</td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="2">キロフプリミエール</td>
+                        <td>Sadler's Wells</td>
+                    </tr>
+                    <tr>
+                        <td>Querida</td>
+                    </tr>
                 </tbody>
 
             </table>
