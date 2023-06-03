fd2fa82f8367d8672a397778887edf177188d2c7
arubaito <asdfghjk0330@gmail.com>
2023-06-03 10:56:23 +0900

    レースメモ画面に表示していた表にCSSを付与

--- components/K02_Syussouhyou.jsx
+++ components/K02_Syussouhyou.jsx
@@ -6,7 +6,7 @@ export default function Syussouhyou({raceKekkaList}){
         <table className={styles.table}>
             {/* テーブルのヘッダ */}
             <thead>
-                <tr>
+                <tr className={styles.header}>
                     <td>着順</td>
                     <td>枠</td>
                     <td>馬番</td>
@@ -30,21 +30,33 @@ export default function Syussouhyou({raceKekkaList}){
 function RaceKekkaTableBody({raceKekkaList}){
     return (
         <tbody>
-            {/* リストを<tr>でマークアップ */}
+            {/* 各レコードを生成 */}
             {raceKekkaList.map(({
                 tyakuzyun, waku, umaban, yosou, bamei, seirei, kisyu, ninki
-            }) => (
-                <tr key={umaban}>
-                    <td>{tyakuzyun}</td>
-                    <td>{waku}</td>
-                    <td>{umaban}</td>
-                    <td>{yosou}</td>
-                    <td>{bamei}</td>
-                    <td>{seirei}</td>
-                    <td>{kisyu}</td>
-                    <td>{ninki}</td>
-                </tr>
-            ))}
+            }) => {
+                return(
+                    <tr key={umaban}>
+                        <td className={styles.tyakuzyun}>{tyakuzyun}</td>
+                        <td className={`${styles.waku} ${
+                            (waku == 1) ? styles.waku :
+                            (waku == 2) ? styles.waku2 :
+                            (waku == 3) ? styles.waku3 :
+                            (waku == 4) ? styles.waku4 :
+                            (waku == 5) ? styles.waku5 :
+                            (waku == 6) ? styles.waku6 :
+                            (waku == 7) ? styles.waku7 :
+                            styles.waku8                        
+                        }`}>
+                        {waku}</td>
+                        <td className={styles.umaban}>{umaban}</td>
+                        <td className={styles.yosou}>{yosou}</td>
+                        <td className={styles.bamei}>{bamei}</td>
+                        <td className={styles.seirei}>{seirei}</td>
+                        <td className={styles.kisyu}>{kisyu}</td>
+                        <td className={styles.ninki}>{ninki}</td>
+                    </tr>
+                )
+        })}
         </tbody>
     );
 }
