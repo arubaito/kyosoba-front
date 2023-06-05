d87b138b33cc32cbbd516c31b289981dd3fc3f40
arubaito <asdfghjk0330@gmail.com>
2023-06-05 09:59:53 +0900

    (K02)レース結果画面のレース名箇所のスタイルを整理

--- pages/raceKekka/index.jsx
+++ pages/raceKekka/index.jsx
@@ -1,17 +1,41 @@
 import RaceKekkaMemo from "components/K02_RaceKekkaMemo";
 import Syussouhyou from "components/K02_Syussouhyou";
 import styles from "styles/page/K02_RaceKekka.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faCloud } from "@fortawesome/free-solid-svg-icons";
+import { faCloudShowersHeavy } from "@fortawesome/free-solid-svg-icons";
+import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
+import { faSnowman } from "@fortawesome/free-solid-svg-icons";
+import { faSnowflake } from "@fortawesome/free-solid-svg-icons";
 
 /*
     (K02)レース結果メモ画面
     getServerSidePropsでサーバから取得してきた競走馬の情報を表示
  */
-export default function RaceKekka({raceKekkaList, raceName, raceGrade}){
+export default function RaceKekka({
+    raceKekkaList, raceName, raceGrade, raceDate, kyori, tousu, tenko, baba
+}){
     return (
         <div className={styles.raceContentsBox}>
-            <div>
-                <h1>{raceName}</h1>
-                <span>{raceGrade}</span>
+            <div className={styles.gridContainer}>
+                <div className={styles.raceGrade}>{raceGrade}</div>
+                <h1 className={styles.raceName}>{raceName}</h1>
+                <div>
+                    <span className={styles.raceDate}>{raceDate}</span>
+                    <span className={styles.kyori}> / {kyori}m</span>
+                    <span className={styles.tenko}> / 天候：
+                                    {
+                                    tenko == 1 ? <FontAwesomeIcon icon={faSun} />
+                                    : tenko == 2 ? <FontAwesomeIcon icon={faCloud} />
+                                    : tenko == 3 ? <FontAwesomeIcon icon={faCloudShowersHeavy} />
+                                    : tenko == 4 ? <FontAwesomeIcon icon={faCloudRain} />
+                                    : tenko == 5 ? <FontAwesomeIcon icon={faSnowman} /> 
+                                    : <FontAwesomeIcon icon={faSnowflake} />
+                                    }</span>
+                    <span className={styles.tousu}> / {tousu}頭</span>
+                    <span className={styles.baba}> / 馬場:{baba}</span>
+                </div>
             </div>
             <Syussouhyou raceKekkaList={raceKekkaList} />
             {/* メモ入力欄 */}
