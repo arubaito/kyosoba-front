ec94db6b9ad592e9268e67d9f01d87c3cdf7ba1a
arubaito <asdfghjk0330@gmail.com>
2023-06-05 23:03:34 +0900

    (K02)react-selectを使って予想記号を選択できるセレクトボックスを追加

--- components/K02_Syussouhyou.jsx
+++ components/K02_Syussouhyou.jsx
@@ -1,5 +1,11 @@
 import styles from "styles/page/K02_Syussouhyou.module.css";
-
+import { useState } from "react";
+import Select from "react-select";
+import { GrEmptyCircle } from "react-icons/gr"
+import { FiTriangle, FiCircle } from "react-icons/fi";
+import { BsFillTriangleFill } from "react-icons/bs";
+import { RxCross1 } from "react-icons/rx";
+import { AiOutlineStar } from "react-icons/ai";
 
 export default function Syussouhyou({raceKekkaList}){
     return (
@@ -22,12 +28,46 @@ export default function Syussouhyou({raceKekkaList}){
     );
 }
 
+// 予想セレクトボックスの選択肢(react-selectで使う)
+const options = [
+    {
+        value: "未選択",
+        label:"--",
+    },
+    {
+        value: "本命",
+        label:<GrEmptyCircle />,
+    },
+    {
+        value: "対抗",
+        label:<FiCircle />,
+    },
+    {
+        value: "単穴",
+        label:<BsFillTriangleFill />,
+    },
+    {
+        value: "連下",
+        label:<FiTriangle />,
+    },
+    {
+        value: "バツ",
+        label:<RxCross1 />,
+    },
+    {
+        value: "穴馬",
+        label:<AiOutlineStar />,
+    },
+];
+
 /**
  * 
  * @param APIから送られてくるレースに出走した馬とその結果のリスト 
  * @returns <tr>でマークアップされた各要素
  */
 function RaceKekkaTableBody({raceKekkaList}){
+    const [selectedValue, setSelectedValue] = useState(options[0]);
+    
     return (
         <tbody>
             {/* 各レコードを生成 */}
@@ -49,7 +89,18 @@ function RaceKekkaTableBody({raceKekkaList}){
                         }`}>
                         {waku}</td>
                         <td className={styles.umaban}>{umaban}</td>
-                        <td className={styles.yosou}>{yosou}</td>
+                        <td className={styles.yosou}>
+                            <Select
+                                id="yosouSelectBox"
+                                instanceId="yosouSelectBox"
+                                options={options}
+                                defaultValue={selectedValue}
+                                onChange={(value) => {
+                                    console.log(value);
+                                    value ? setSelectedValue(value) : null;
+                                }}
+                            />
+                        </td>
                         <td className={styles.bamei}>{bamei}</td>
                         <td className={styles.seirei}>{seirei}</td>
                         <td className={styles.kisyu}>{kisyu}</td>
