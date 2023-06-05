ac91f2621f99e5d0b615acb5949293f86ccbdf66
arubaito <asdfghjk0330@gmail.com>
2023-06-05 12:26:44 +0900

    (K02)レース結果の初期表示時に登録されているメモを表示するように修正

--- components/K02_RaceKekkaMemo.jsx
+++ components/K02_RaceKekkaMemo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "styles/page/K02_RaceKekkaMemo.module.css";
 
 /**
@@ -7,9 +7,19 @@ import styles from "styles/page/K02_RaceKekkaMemo.module.css";
  * 入力フォームの参考⇒https://reffect.co.jp/react/react-hook-form 
  */
 export default function RaceKekkaMemo({raceZisshiId}){
-  
+
     // useStateを使って入力フォームを管理
     const [raceKekkaMemo, setRaceKekkaMemo] = useState(""); // 初期値 
+    
+    // DBからレース結果メモを取得してレースメモに初期値をセット
+    useEffect(() => {
+        (async () => {
+            const initialRaceKekkaMemo = await getRaceKekkaMemo(raceZisshiId);
+            console.log(initialRaceKekkaMemo);
+            console.log(initialRaceKekkaMemo.raceKekkaMemo);
+            setRaceKekkaMemo(initialRaceKekkaMemo.raceKekkaMemo);
+        })()
+    }, []);
 
     // 入力欄が更新されたらstate値を更新
     const handleChangeRaceKekkaMemo = (e) => {
@@ -20,10 +30,6 @@ export default function RaceKekkaMemo({raceZisshiId}){
     const handleSubmit = async (e) => {
         
         e.preventDefault();
-        console.log({
-            raceZisshiId, 
-            raceKekkaMemo
-        });
 
         // 登録・更新用のAPI呼び出し
         const response = await fetch("http://localhost:8080/register-race-kekka-memo",{ 
@@ -59,4 +65,20 @@ export default function RaceKekkaMemo({raceZisshiId}){
             </form>
         </div>
     );
+}
+
+/**
+ * レース実施IDをもとにDBに登録されてるレース結果メモをオブジェクト形式で取得
+ * 
+ * @param {*} raceZisshiId : レース実施ID
+ * @returns レース結果メモ
+ */
+async function getRaceKekkaMemo(raceZisshiId){
+    try{
+        const response = await fetch(`http://localhost:8080/get-race-kekka-memo-${raceZisshiId}`);
+        const res = await response.json();
+        return res;
+    }catch(err){
+        console.log(err);
+    }
 }
