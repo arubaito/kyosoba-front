2a5130f3aaa9ba7a3d0d638a989d94149c476001
arubaito <asdfghjk0330@gmail.com>
2023-06-07 18:42:44 +0900

    (K01)見出しのタイトルがなかったので追加

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
 import Ichiran from "components/K01_ichiran";
 import Kettouhyou from "components/K01_kettouhyou";
+import TitleAndIcon from "components/common/titleAndIcon";
 import Modal from "components/modal";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHorseHead } from "@fortawesome/free-solid-svg-icons";
+import styles from "styles/page/K01_KyosobaIchiran.module.css"
 
-export default function Home() {
+/**
+ * (K01)競走馬一覧ページ
+ */
+export default function KyosobaIchiran() {
 
   // state変数でモーダルウィンドウの表示非表示を切り替える
   const [show, setShow] = useState(false);
 
   return (
-      <div>
+      <div className={styles.kyousoubaIchiranContentsBox}>
+        <TitleAndIcon title="競走馬一覧">
+          <FontAwesomeIcon icon={faHorseHead} />
+        </TitleAndIcon>
         {/* 競走馬の一覧 */}
         <Ichiran setShow={setShow}></Ichiran>
 
