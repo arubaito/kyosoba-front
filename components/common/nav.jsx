f64dcc57ffcf0a0f3da500a6a36989bb5ba2d6dc
arubaito <asdfghjk0330@gmail.com>
2023-06-09 07:59:50 +0900

    (共通)ナビゲーションメニューにアイコンを追加

--- components/common/nav.jsx
+++ components/common/nav.jsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import styles from "styles/common_components/nav.module.css";
+import { GiHorseshoe } from "react-icons/gi"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons"; 
 
 /* ナビゲーションメニューを表示するコンポーネント */
 export default function Nav(){
@@ -7,13 +10,15 @@ export default function Nav(){
         <nav>
             <ul className={styles.list}>
                 <li>
-                    <Link href="/raceIchiran">レース結果一覧</Link>
+                    <GiHorseshoe />
+                    <Link href="/raceIchiran" className={styles.navText}>レース一覧</Link>
                 </li>
                 <li>
                     <Link href="/">メニュー２</Link>
                 </li>
                 <li>
-                    <Link href="/kanri">管理メニュー</Link>
+                    <FontAwesomeIcon icon={faGear} />
+                    <Link href="/kanri" className={styles.navText}>管理メニュー</Link>
                 </li>
             </ul>
         </nav>
