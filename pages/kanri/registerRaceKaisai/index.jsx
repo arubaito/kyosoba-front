b11352a5c02abde04d179fb88482a6b0bac25e1b
arubaito <asdfghjk0330@gmail.com>
2023-06-12 08:51:03 +0900

    (K05)レース実施概要にAPIをたたいて実際に登録するように修正

--- pages/kanri/registerRaceKaisai/index.jsx
+++ pages/kanri/registerRaceKaisai/index.jsx
@@ -73,7 +73,7 @@ function RegisterForm({raceMasterList}){
         mode: 'onSubmit',
         reValidateMode: 'onChange',
         defaultValues: {
-            raceName: '',
+            // raceName: '',
         }
     })
 
@@ -112,19 +112,17 @@ function RegisterForm({raceMasterList}){
     ];
 
     // 登録ボタンのハンドラ
-    const handleOnSubmit = async (e) => {
-        e.preventDefault();
-        console.log("handleOnsubmit");
-        console.log(e)
-        // registerRaceMaster(values);
+    const handleOnSubmit = async (values) => {
+        console.log(values)
+        await registerRaceZisshi(values);
     }
 
     return (
         <>
-            <form className={styles.form} onSubmit={handleOnSubmit} >
+            <form className={styles.form} onSubmit={handleSubmit(handleOnSubmit)} >
                 <FormProvider register={register} formState={formState}>
                     {/* レース名 */}
-                    <FormSelect labelName="レース名" formName="raceName" optionsList={optionsRaceList} />
+                    <FormSelect labelName="レース名" formName="raceId" optionsList={optionsRaceList} />
 
                     {/* 日付 */}
                     <FormInputDate required="true" labelName="日付" formName="kaisaiDate" />
@@ -136,7 +134,7 @@ function RegisterForm({raceMasterList}){
                     <FormSelect labelName="馬場" formName="baba" optionsList={optionsBabaList}/>
 
                     {/* 頭数 */}
-                    <FormInputNumber required="true" labelName="頭数" formName="tosu" />
+                    <FormInputNumber required="true" labelName="頭数" formName="tousu" />
 
                     {/* 送信ボタン */}
                     <FormButton buttonName="登録"/>
@@ -146,4 +144,33 @@ function RegisterForm({raceMasterList}){
     );
 }
 
+/**
+ * レース実施概要を登録するAPIを呼び出す関数
+ * @param {*} param0 
+ */
+async function registerRaceZisshi({raceId, kaisaiDate, tenko, baba, tousu}){
 
+    console.log("#registerRaceZisshi");
+
+    // API呼び出し
+    const response = await fetch("http://localhost:8080/register-race-zisshi", {
+        method: "POST", 
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            raceId,
+            kaisaiDate,
+            tenko,
+            baba,
+            tousu,
+        })
+    });
+
+    if(response.status === 200){
+        alert("保存しました。");
+    } else{
+        alert("保存できませんでした。管理者にご連絡ください。");
+    }
+
+}
